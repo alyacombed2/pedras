22fe006419cfe9ca29f76e58656d30466e78805c
alyacombed2 <premiuresso019@gmail.com>
2026-02-12 22:03:44 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,9 @@
+let curtidas = 0;
+
 const btn = document.getElementById("curtirBtn");
 const display = document.getElementById("curtidas");
 
-
-const namespace = "pedras-noticias-online";
-const key = "likes";
-
-
-fetch(`https://api.countapi.xyz/get/${namespace}/${key}`)
-  .then(response => response.json())
-  .then(data => {
-    const likes = data.value || 0;
-    display.textContent = `${likes} curtida${likes > 1 ? "s" : ""}`;
-  });
-
-// quando clica no botão
 btn.addEventListener("click", () => {
-  fetch(`https://api.countapi.xyz/hit/${namespace}/${key}`)
-    .then(response => response.json())
-    .then(data => {
-      display.textContent = `${data.value} curtida${data.value > 1 ? "s" : ""}`;
-    });
+    curtidas++;
+    display.textContent = `${curtidas} curtida${curtidas > 1 ? "s" : ""}`;
 });
