f426893538cc70121d84901a432fc8acdc1a3a70
alyacombed2 <premiuresso019@gmail.com>
2026-02-12 21:51:04 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,24 @@
-let curtidas = 0;
-
 const btn = document.getElementById("curtirBtn");
 const display = document.getElementById("curtidas");
 
+
+const namespace = "noticias-online-alya";
+const key = "likes";
+
+
+fetch(`https://api.countapi.xyz/get/${namespace}/${key}`)
+  .then(res => res.json())
+  .then(data => {
+    const likes = data.value || 0;
+    display.textContent = `${likes} curtida${likes > 1 ? "s" : ""}`;
+  });
+
+
 btn.addEventListener("click", () => {
-    curtidas++;
-    display.textContent = `${curtidas} curtida${curtidas > 1 ? "s" : ""}`;
+  fetch(`https://api.countapi.xyz/hit/${namespace}/${key}`)
+    .then(res => res.json())
+    .then(data => {
+      const likes = data.value;
+      display.textContent = `${likes} curtida${likes > 1 ? "s" : ""}`;
+    });
 });
