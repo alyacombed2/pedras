1fc1c0f3d3c61e25e272d4ef59dd0901690e17f2
alyacombed2 <premiuresso019@gmail.com>
2026-02-12 21:55:42 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,12 @@ const btn = document.getElementById("curtirBtn");
 const display = document.getElementById("curtidas");
 
 
-const namespace = "noticias-online-alya";
+const namespace = "pedras-noticias-online";
 const key = "likes";
 
 
 fetch(`https://api.countapi.xyz/get/${namespace}/${key}`)
-  .then(res => res.json())
+  .then(response => response.json())
   .then(data => {
     const likes = data.value || 0;
     display.textContent = `${likes} curtida${likes > 1 ? "s" : ""}`;
@@ -16,9 +16,8 @@ fetch(`https://api.countapi.xyz/get/${namespace}/${key}`)
 
 btn.addEventListener("click", () => {
   fetch(`https://api.countapi.xyz/hit/${namespace}/${key}`)
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => {
-      const likes = data.value;
-      display.textContent = `${likes} curtida${likes > 1 ? "s" : ""}`;
+      display.textContent = `${data.value} curtida${data.value > 1 ? "s" : ""}`;
     });
 });
